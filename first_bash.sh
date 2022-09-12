ef946b516a186680ebee6d03bffc2564a0cc3f50
mdhb9717 <mdhb9171@gmail.com>
2022-09-12 11:04:42 +0430

    update7

--- first_bash.sh
+++ first_bash.sh
@@ -18,7 +18,7 @@ sudo apt install software-properties-common ca-certificates lsb-release apt-tran
 sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
 sudo apt update
 sudo apt install --no-install-recommends php8.1
-sudo apt-get install -y php8.1-cli php8.1-common php8.1-mysql php8.1-zip php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
+sudo apt-get install -y php8.1-cli php8.1-common php8.1-mysql php8.1-gd php8.1-mbstring php8.1-curl php8.1-xml php8.1-bcmath
 
 # installing composer
 cd ~
